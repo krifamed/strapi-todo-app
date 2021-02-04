640b85b133b0ea228156b99215a7f0e53ab21f30
krifamed <mohamed.krifa02@gmail.com>
2021-02-04 17:32:50 +0000

    add completeAll and mark_as_completed controller

--- api/todos/controllers/todos.js
+++ api/todos/controllers/todos.js
@@ -1,8 +1,24 @@
 'use strict';
+const {sanitizeEntity} = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async clear_completed(ctx){
+        let entries = await strapi.query('todos').delete({active: false});
+        return entries.map(entry => sanitizeEntity(entry, {model : strapi.models.todos}));
+    },
+
+    
+    async toggle_completed(ctx){
+        const {active} = ctx.request.body; 
+        const todos = await strapi.query('todos').find({});
+        const entries = await Promise.all(todos.map(async entry => {
+            return await strapi.query('todos').update({id: entry.id}, {active: active});
+        }));
+        return entries.map(entry => sanitizeEntity(entry, {model : strapi.models.todos}));
+    }
+};
